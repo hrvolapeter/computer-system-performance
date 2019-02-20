c041123ebf59a417de12a0b3959fcda9dd58bb5a
retep007 <peter.hrvola@airtame.com>
2019-02-20 20:12:13 +0100

    Stream

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -28,6 +28,11 @@ fn compute_file(content: &str) -> [f64; 12] {
     res
 }
 
+fn records_for_path(path: &str) -> impl ParallelIterator<Item=csv::StringRecord> {
+    let rdr = csv::ReaderBuilder::new().from_path(path).unwrap();
+    rdr.into_records().filter_map(Result::ok).par_bridge()
+} 
+
 fn read_file(file: &str) -> String {
     println!("Reading file {}", file);
     let mut contents = String::new();
@@ -39,25 +44,36 @@ fn read_file(file: &str) -> String {
 
 fn main() {
     let now = Instant::now();
-    let files = get_file_paths();
     println!(
         "Elapsed time loading: {}s {}ms",
         now.elapsed().as_secs(),
         now.elapsed().subsec_millis()
     );
     let now = Instant::now();
-    let res: Vec<[f64; 12]> = files.par_iter()
-        .map(|content| compute_file(&content[..]))
-        .collect();
-
-    let res: [f64; 12] = res.iter()
-        .fold([0.0; 12], |sum, val| {
+    let files = get_file_paths();
+    
+    let res = files.par_iter()
+        .map(|content| records_for_path(&content[..]))
+        .flatten()
+        .filter(|x| f64::from_str(x.get(4).expect("Format")).unwrap_or(0.0) > 1.0) // miles
+        .filter(|x| f64::from_str(x.get(3).expect("Format")).unwrap_or(0.0) > 60.0) // time s
+        .filter(|x| x.get(14).expect("Format") == "Cash") // time s
+        .map(|x| {
+            let date = NaiveDateTime::parse_from_str(x.get(1).expect("Format"), "%Y-%m-%d %H:%M:%S").expect("invalid time");
+            (date.month() - 1, f64::from_str(x.get(13).expect("Format")).unwrap_or(0.0))
+        })
+        .fold(|| [0.0; 12], |mut part, (month, a)| {
+            part[month as usize] = part[month as usize] + a;
+            part
+        }).
+        reduce(|| [0.0; 12], |a, b| {
             let mut res = [0.0; 12];
-            for i in 0..sum.len() {
-                res[i] = sum[i] + val[i];
+            for i in 0..12 {
+                res[i] = a[i] + b[i];
             }
             res
         });
+            
     println!(
         "Elapsed time: {}s {}ms",
         now.elapsed().as_secs(),
@@ -71,6 +87,5 @@ fn get_file_paths() -> Vec<String> {
     glob::glob(&file_path[..])
         .expect("Glob should succeed")
         .map(|m| format!("{}", m.expect("Should be path").display()))
-        .map(|file| read_file(&file[..]))
         .collect()
 }
