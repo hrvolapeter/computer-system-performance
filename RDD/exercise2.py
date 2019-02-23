2f6066e77ba56c6ffa806f901a39202dc771e0c2
Zoltán Fraknói <fraknoiz@gmail.com>
2019-02-23 17:08:51 +0100

    RDD query done

--- RDD/exercise2.py
+++ RDD/exercise2.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pyspark.sql.types import *
 import time
 
-
+totalParseErrors = 0
 sc = SparkContext('local[*]')
 spark = SparkSession(sc)
 
@@ -32,70 +32,57 @@ schema = StructType([
     StructField("dropoff_longitude", IntegerType())
 ])
 
+
 def parseData(fields):
+    global totalParseErrors
 
-    # trip_start = datetime.strptime("2016-1-13 06:15:00", '%Y-%m-%d %H:%M:%S')
+    trip_start = fields[1]
+    trip_end = fields[2]
 
-    # try:
-    #     trip_start = datetime.strptime(fields[1], "%Y-%m-%d %H:%M:%S")
-    # except:
-    #     trip_start = datetime.strptime("1900-1-1 00:00:00", "%Y-%m-%d %H:%M:%S")
-    #     print("--------------PARSE ERROR-----------------")
-    #     print(fields)
-    #     print("------------------------------------------")
-
-    # try:
-    #     trip_end = datetime.strptime(fields[2], "%Y-%m-%d %H:%M:%S")
-    # except:
-    #     trip_end = datetime.strptime("1900-1-1 00:00:00", "%Y-%m-%d %H:%M:%S")
-    #     print("--------------PARSE ERROR-----------------")
-    #     print(fields)
-    #     print("------------------------------------------")
+    try:
+        trip_start_month = fields[1].month
+    except:
+        totalParseErrors += 1
+        # print("--------------PARSE ERROR-----------------")
+        # print(fields)
+        # print("------------------------------------------")
+        trip_start_month = 1
 
 
     try:
-        trip_start = fields[1]
+        trip_end_month = fields[2].month
     except:
-        print("--------------PARSE ERROR-----------------")
-        print(fields)
-        print("------------------------------------------")
-        trip_start = datetime.strptime("1900-1-1 00:00:00", "%Y-%m-%d %H:%M:%S")
-
-
-    try:
-        trip_end = fields[2]
-    except:
-        print("--------------PARSE ERROR-----------------")
-        print(fields)
-        print("------------------------------------------")
-        trip_end = datetime.strptime("1900-1-1 00:00:00", "%Y-%m-%d %H:%M:%S")
-
-
-    trip_start_month = fields[1].month
-    trip_end_month = fields[2].month
+        totalParseErrors += 1
+        # print("--------------PARSE ERROR-----------------")
+        # print(fields)
+        # print("------------------------------------------")
+        trip_end_month = 1
 
 
     try:
         trip_seconds = int(fields[3])
     except:
-        print("--------------PARSE ERROR-----------------")
-        print(fields)
-        print("------------------------------------------")
+        totalParseErrors += 1
+        # print("--------------PARSE ERROR-----------------")
+        # print(fields)
+        # print("------------------------------------------")
         trip_seconds = 0
 
     try:
         trip_miles = float(fields[4])
     except:
+        totalParseErrors += 1
         trip_miles = 0
 
     try:
         trip_total = float(fields[13])
     except:
+        totalParseErrors += 1
         trip_total = 0
 
     payment_type = str(fields[14])
 
-    return (trip_end_month, trip_seconds, trip_miles, trip_total, payment_type)
+    return (trip_start_month, trip_seconds, trip_miles, trip_total, payment_type)
 
 
 #path = "chicago_taxi_trips_2016_*.csv"
@@ -114,32 +101,45 @@ df = (spark.read
             .option("delimiter", ",")
             .schema(schema)
             .load(path)
-
         )
 
 
 myRdd = df.rdd
 
-paymentData = myRdd.map(parseData).filter(lambda x: x[1] > 60).filter(lambda y: y[2] > 1.0).filter(lambda z: "Cash" in z[4]).groupBy(lambda a: a[0])
-
-
-# TODO: fucking finish it. it doesn't even get here, it just crashed on the previous step
-
-print(paymentData.first())
-# cashonly = paymentData.map(lambda x: x[0])
-
 time2 = time.time()
 
-# total = paymentData.sum()
+myRdd.count()
 
 time3 = time.time()
 
-print("Total payments with cash:")
-print(total)
+# paymentData = myRdd.map(parseData).filter(lambda x: x[1] > 60).filter(lambda y: y[2] > 1.0).filter(lambda z: "Cash" in z[4]).groupBy(lambda a: a[0])
+paymentData = (myRdd.map(parseData)
+                    .filter(lambda x: x[1] > 60)
+                    .filter(lambda y: y[2] > 1.0)
+                    .filter(lambda z: "Cash" in z[4])
+                    .map(lambda a: ( a[0], a[3] ))
+                    .reduceByKey(lambda c, d: c+d)
+              )
+
+time4 = time.time()
 
 
-# print("loading time: ")
-# print(time2 - time1)
+
+
+# print("Total parse errors:", totalParseErrors)
+
+
+
+
+# print("Total payments with cash:")
+# print(total)
+
 print("\r\n")
-print("Calculation time (seconds): ")
-print(time3-time2)
+
+print(paymentData.take(12))
+
+print("\r\n")
+
+print("Dataframe 'load' time: ", time2 - time1)
+print("RDD.count time: ", time3 - time2)
+print("RDD Calculation time (seconds): ", time4 - time3)
